7432f95564f6770296562e206eceb6467902c596
Ahmed-Elawad <a.elawad97@gmail.com>
2020-10-07 10:18:31 -0400

    Server to route all request for service rendering

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const express = require('express');
-const app = express();
+const express = require('express'),
+    { createProxyServer } = require('http-proxy');
 
+const targets = {
+    productView: 'http://localhost:3002',
+    review: 'http://localhost:3003',
+    relatedFurnitures: 'http://localhost:3001',
+    productOptions: 'http://localhost:3000'
+}
+const app = express();
+const productViewProxy = createProxyServer({target: 'http://localhost:3000'});
 app.use(express.static('public'));
-app.use(function(req, res) {
-    res.header("Access-Control-Allow-Origin", '*');
-})
+app.use('/', (req, res) => {
+    if (req.url === '/api/reviews/15') {
+        productViewProxy.web(req, res, {target: targets.review})
+    } else if (req.url === '/relatedFurnitures') {
+        productViewProxy.web(req, res, {target: targets.relatedFurnitures})
+    } else {
+        productViewProxy.web(req, res, {target: 'http://localhost:3002/'})
+    }
+});
 app.listen(9000, () => console.log(`proxy running on port: 9000`));
