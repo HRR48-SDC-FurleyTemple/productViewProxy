1c4cf8161081e0d29e85f3c76760432edb703525
Ahmed-Elawad <a.elawad97@gmail.com>
2020-10-08 10:41:54 -0400

    Installed styling dep and updated minor sytax on proxy

--- index.js
+++ index.js
@@ -7,16 +7,20 @@ const targets = {
     relatedFurnitures: 'http://localhost:3001',
     productOptions: 'http://localhost:3000'
 }
+
 const app = express();
 const productViewProxy = createProxyServer({target: 'http://localhost:3000'});
 app.use(express.static('public'));
 app.use('/', (req, res) => {
     if (req.url === '/api/reviews/15') {
         productViewProxy.web(req, res, {target: targets.review})
-    } else if (req.url === '/relatedFurnitures') {
+    } else if (req.url === '/api/similarProducts/products/1') {
         productViewProxy.web(req, res, {target: targets.relatedFurnitures})
+    } else if(req.url === '/api/productOptions/products/1') {
+        productViewProxy.web(req, res, {target: productOptions})
     } else {
         productViewProxy.web(req, res, {target: 'http://localhost:3002/'})
     }
 });
+
 app.listen(9000, () => console.log(`proxy running on port: 9000`));
