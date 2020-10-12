7b520996d14e9fc8c9dc0047e635be9273af3b00
Ahmed-Elawad <a.elawad97@gmail.com>
2020-10-12 11:36:48 -0400

    Proxy final FEC

--- public/SPA/app.jsx
+++ public/SPA/app.jsx
@@ -2,6 +2,8 @@ import React, { useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import styles from './styled.js';
 const AppContainer = styles.AppContainer;
+const SectionContainer = styles.SectionContainer;
+const Page = styles.Page;
 
 const App = () => {
 
@@ -10,10 +12,10 @@ const App = () => {
         const productViewScript = document.createElement('script');
         const bottomCarouselScript = document.createElement('script');
         const reviewsScript = document.createElement('script');
-        productOptionsScript.src = "http://localhost:3002/bundle.js";
-        productViewScript.src = "http://localhost:3000/main.js";
-        bottomCarouselScript.src = "http://localhost:3003/bundle.js";
-        reviewsScript.src = "http://localhost:3001/bundle.js";
+        productOptionsScript.src = "http://localhost:3000/main.js";
+        productViewScript.src = "http://localhost:3002/bundle.js";
+        bottomCarouselScript.src = "http://localhost:3001/bundle.js";
+        reviewsScript.src = "http://localhost:3003/bundle.js";
         document.body.appendChild(productOptionsScript);
         document.body.appendChild(productViewScript);
         document.body.appendChild(bottomCarouselScript);
@@ -21,13 +23,12 @@ const App = () => {
     })
     return (
         <AppContainer>
-            <div id="productView"></div>
-            <div id="app"></div>
-            <div id="reviews"></div>
-            <div id="BottomCarousel"></div>
+            <SectionContainer gridArea="productView" id="productView" />
+            <SectionContainer options gridArea="productOptionsApp" id="productOptionsApp" />
+            <SectionContainer gridArea="review-module" id="review-module" />
+            <SectionContainer gridArea="BottomCarousel" id="BottomCarousel" />
         </AppContainer>
     )
 };
-
 
 ReactDOM.render(<App />, document.getElementById('mainPage'))
--- public/SPA/styled.js
+++ public/SPA/styled.js
@@ -1,14 +1,24 @@
 import Styled from 'styled-components';
 
 const styles = {
+    Page: Styled.div`
+        margin: auto auto;
+    `,
     AppContainer: Styled.div`
         display: grid;
         grid-template-areas:
-            "productView app"
-            "productView app"
-            "productView app"
-            "reviews app"
-            "BottomCarousel app"
+            "productView productOptionsApp"
+            "review-module productOptionsApp"
+            "BottomCarousel productOptionsApp"
+    `,
+    SectionContainer: Styled.div`
+        grid-area: ${(props) => props.gridArea};
+        ${(props) => {
+            let stickyOptions = `position: -webkit-sticky; /* Safari */
+            position: sticky;
+            top: 0;`;
+            return props.options ? stickyOptions : '';
+        }}
     `
 };
 
